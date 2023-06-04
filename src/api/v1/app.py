6bb74ca5a9b5ccee76f6a17212d87774e8244ac2
Gizachew Bayness <eleccrazy24@gmail.com>
2023-06-04 15:39:34 +0300

    README file updated

--- src/api/v1/app.py
+++ src/api/v1/app.py
@@ -41,6 +41,6 @@ def tear_down_db(execute):
 
 
 if __name__ == "__main__":
-    app.run(host=getenv('KETERO_API_PORT', '0.0.0.0'),
-            port=getenv('KETERO_API_PORT ', 5000),
+    app.run(host=getenv('KETERO_API_ADDRESS', '0.0.0.0'),
+            port=getenv('KETERO_API_PORT ', 5001),
             threaded=True, debug=True)
